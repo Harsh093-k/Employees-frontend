1d21d8c133c83911bed2b2cc435457213518416a
Harsh093-k <harshkashyap701153@gmail.com>
2025-04-18 15:29:14 +0530

    done

--- backend/controller/EmployeesController.js
+++ backend/controller/EmployeesController.js
@@ -252,7 +252,7 @@ export const login = async (req, res) => {
    
     return res.cookie('token', token, {
       httpOnly: true,
-      sameSite: 'none',
+      sameSite: 'static',
       secure: true, 
       maxAge: 1 * 24 * 60 * 60 * 1000
     }).json({
