919d9f452ec4b11b85a3f44acea90a9c33663859
Harsh093-k <harshkashyap701153@gmail.com>
2025-04-26 10:31:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ const port = 8080;
 app.use(express.json())
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://employees-frontend-delta.vercel.app',
+    // origin: 'https://employees-frontend-delta.vercel.app',
+      origin: 'http://localhost:3000',
     credentials: true
 }
 app.use(cors(corsOptions));
